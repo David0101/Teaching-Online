53fecbd4eb1b4d88b507158020f39d8e46272d75
jim <ljw5332545>
2020-09-22 18:06:38 +0800

    MemberHandler

---
+++ online-common/src/main/java/com/jim/common/core/module/ModuleId.java
@@ -0,0 +1,22 @@
+package com.jim.common.core.module;
+
+/**
+ * @program: teaching-online
+ * @author: jim
+ * @create: 2020-09-22 15:16
+ */
+public interface ModuleId {
+    /**
+     * 心跳模块
+     */
+    short HEART_BEAT = 0;
+    /**
+     * 会员模块
+     */
+    short MEMBER = 1;
+
+    /**
+     * 画板轨迹模块
+     */
+    short TRACK = 2;
+}
---
+++ online-netty/src/main/java/com/jim/online/module/member/MemberHandler.java
@@ -0,0 +1,13 @@
+package com.jim.online.module.member;
+
+import com.jim.common.core.annotion.SocketModule;
+import com.jim.common.core.module.ModuleId;
+
+/**
+ * @program: teaching-online
+ * @author: jim
+ * @create: 2020-09-22 14:52
+ */
+@SocketModule(module = ModuleId.MEMBER)
+public class MemberHandler {
+}
